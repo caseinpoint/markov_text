ff3c6de0d075e4e5b0b733c12fb67fa5861b2289
caseinpoint <drues.other.email@gmail.com>
2024-06-07 12:52:56 -0600

    completed train_on_file and train_on_directory

--- markov.py
+++ markov.py
@@ -1,5 +1,7 @@
 from collections import Counter
 from dataclasses import dataclass, field
+from glob import iglob
+from os import PathLike
 
 
 @dataclass
@@ -30,3 +32,23 @@ class MarkovChain:
 				self.chain[key] = MarkovLink(is_cap=is_cap)
 
 			self.chain[key].word_weights.update({words[i + self.ngram]: 1})
+
+	def train_on_file(self, file_path: PathLike) -> None:
+		"""Read file_path and train chain on text."""
+
+		with open(file=file_path, mode='r') as f:
+			text = f.read()
+
+		self.train(src=text)
+
+	def train_on_directory(self, dir_path: PathLike, recursive: bool = False) -> None:
+		"""Read every file in dir_path and train chain on text."""
+
+		for file_path in iglob(pathname=dir_path, recursive=recursive):
+			print(file_path)
+			self.train_on_file(file_path=file_path)
+
+	def capital_keys(self) -> list:
+		"""Get all keys that begin with a capital letter."""
+
+		return [key for key,link in self.chain.items() if link.is_cap]
