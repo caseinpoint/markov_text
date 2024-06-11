47e890b162769d38105bcbe0c860faf8a26254d1
caseinpoint <drues.other.email@gmail.com>
2024-06-11 14:17:14 -0600

    successful test of rendering templates

--- server.py
+++ server.py
@@ -2,5 +2,11 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+@app.route('/base')
+def test():
+    """Render the base template as a test."""
+
+    return render_template('base.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
